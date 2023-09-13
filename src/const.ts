822eba7faad2ae4523bfadf29896475bf73345d4
Viktor Stepanov <vitya.zap@gmail.com>
2023-09-13 16:25:08 +0300

    feat: add homepage layout

--- src/const.ts
+++ src/const.ts
@@ -1,1 +1,5 @@
 export const ROUTES = Object.freeze({ HOME: '/', USER: '/:username' });
+export const EXTERNAL_URL = Object.freeze({
+  TWEET: 'https://twitter.com/#!/jeresig/status/33968704983138304',
+  GITHUB: 'https://github.com/',
+});
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,40 @@
+import { type FormEvent } from 'react';
+import { EXTERNAL_URL } from '../const';
+
 export default function Home() {
-  return <div>Home</div>;
+  function handleSubmit(event: FormEvent<HTMLFormElement>): void {
+    event.preventDefault();
+    throw new Error('Function not implemented.');
+  }
+
+  return (
+    <div>
+      <h1>My GitHub Résumé</h1>
+      <p>
+        As a software startup owner I really enjoy when people send us their
+        résumés and they include their github account so we can see tangible
+        work they have done.
+      </p>
+      <p>
+        After a{' '}
+        <a href={EXTERNAL_URL.TWEET} target="_blank">
+          tweet by John Resig
+        </a>{' '}
+        I imagined that it may be nice for people to be able to generate their{' '}
+        <a href={EXTERNAL_URL.GITHUB} target="_blank">
+          GitHub
+        </a>{' '}
+        résumés.
+      </p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder={
+            'Enter your GitHub username and then click "Generate" button or press "Enter"'
+          }
+        />
+        <button type="submit">Generate</button>
+      </form>
+    </div>
+  );
 }
