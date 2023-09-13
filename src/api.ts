8842588d905dcdbd4dd0c8d3712bf74ee0bf2ed0
Viktor Stepanov <vitya.zap@gmail.com>
2023-09-13 20:13:59 +0300

    feat: define API calls

---
+++ src/api.ts
@@ -0,0 +1,15 @@
+import { Octokit } from '@octokit/rest';
+
+const octokit = new Octokit();
+
+export async function getUserData(username: string) {
+  return await octokit.rest.users.getByUsername({ username });
+}
+
+export async function getUserRepos(
+  username: string,
+  page = 1,
+  per_page = 30,
+) {
+  return await octokit.rest.repos.listForUser({ username, page, per_page });
+}
