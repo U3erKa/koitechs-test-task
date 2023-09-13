b69e3c15264713e512ae9542967eb6d3a0871d2c
Viktor Stepanov <vitya.zap@gmail.com>
2023-09-13 20:15:55 +0300

    feat: fetch user data while navigating to a route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Home, User } from './pages';
+import { getUserData, getUserRepos } from './api';
 import { ROUTES } from './const';
 import './App.scss';
 
 const router = createBrowserRouter([
   { path: ROUTES.HOME, element: <Home /> },
-  { path: ROUTES.USER, element: <User /> },
+  {
+    path: ROUTES.USER,
+    element: <User />,
+    async loader({ params }) {
+      const [user, repos] = await Promise.all([
+        getUserData(params.username!),
+        getUserRepos(params.username!, undefined, Number.MAX_SAFE_INTEGER),
+      ]);
+      return [user.data, repos.data];
+    },
+  },
 ]);
 
 function App() {
