c7591649eda94c6d8f215023ddcfa32e092004b6
Viktor Stepanov <vitya.zap@gmail.com>
2023-09-14 12:38:01 +0300

    feat: load additional repos when needed

--- src/components/UserRepos.tsx
+++ src/components/UserRepos.tsx
@@ -1,5 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { getUserRepos } from '../api';
 import { truncateNumber, uniqueId } from '../functions';
+import { INITIALLY_LOADED_REPOS, MAX_REPOS_PER_REQUEST } from '../const';
 import type { Repos } from '../types';
 
 type Props = { username: string; public_repos: number; repos: Repos };
@@ -9,6 +11,25 @@ export default function UserRepos({ username, public_repos, repos }: Props) {
 
   const languages: Record<string, number> = {};
   let languageCount = 0;
+
+  useEffect(() => {
+    const isAllReposLoaded = public_repos <= INITIALLY_LOADED_REPOS;
+    if (!isAllReposLoaded) {
+      const tasks: ReturnType<typeof getUserRepos>[] = [];
+      for (
+        let i = 1;
+        i <= Math.ceil(public_repos / MAX_REPOS_PER_REQUEST);
+        i++
+      ) {
+        tasks.push(getUserRepos(username, i, MAX_REPOS_PER_REQUEST));
+      }
+
+      Promise.all(tasks).then((results) => {
+        const repos = results.flatMap(({ data }) => data);
+        setAllRepos(repos);
+      });
+    }
+  }, [public_repos, username]);
 
   allRepos.forEach(({ language }) => {
     if (language) {
